8b8ef22933374275d1a4e62ac212a1312421e709
qwe <h@gmail.com>
2025-10-22 14:20:16 +0300

    Sākām taisīt Gotema klase

--- src/nesekmigs/Gotema.java
+++ src/nesekmigs/Gotema.java
@@ -1,5 +1,20 @@
 package nesekmigs;
 
 public class Gotema {
-
+	
+	
+	public void main(String[]args) {
+		
+		String izvele;
+		int izvelesID;
+		String[] darbibas = {"Izveidot varoni", "Noņemt varoni", "Varoņu saraksts",
+							 "Varoņa darbības izvēle", "Aizvērt programmu"};
+		//
+		int DZIVSK, HP, SPEKS, VECUMS, AIZS; 
+		String varonuIzvele, VARDS, IZCELSME;
+		
+		
+		
+		
+	}
 }
