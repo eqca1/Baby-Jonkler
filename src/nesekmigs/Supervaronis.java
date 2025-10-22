8645164531cbb25188b76288284cfcbfacb8c0bb
[Emils Glotovs] <[emilsglotovs@sk.lvt.lvgit config user.emailgit config --global user.email [emilsgltoovs@sk.lvt.lv]git config user.emailgit config --global use.email[emils.glotovs@sk.lvt.lv>
2025-10-22 14:29:04 +0300

    Izveidotas klasē supervaronis metodes un konstruktors

--- src/nesekmigs/Supervaronis.java
+++ src/nesekmigs/Supervaronis.java
@@ -3,6 +3,53 @@ package nesekmigs;
 public class Supervaronis {
 
 	private String VARDS, IZCELSME, MOTIVS;
-	private int DZIVSK, HP, SPEKS, varonuIzvele, VECUMS, AIZS;
+	private int DZIVSK, HP, SPEKS, VECUMS, AIZS;
 	
+	 // konstruktors
+	
+    public Supervaronis(int DZIVSK, int HP, int SPEKS, int VECUMS, int AIZS, String VARDS
+    		, String IZCELSME, String MOTIVS) {
+        this.VARDS = VARDS;
+        this.IZCELSME = IZCELSME;
+        this.MOTIVS = MOTIVS;
+        this.DZIVSK = DZIVSK;
+        this.HP = HP;
+        this.SPEKS = SPEKS;
+        this.VECUMS = VECUMS;
+        this.AIZS = AIZS;
+        
+    }
+    
+    // metodes
+    public String VARDS() {
+        return VARDS;
+    }
+    
+    public String IZCELSME() {
+        return IZCELSME;
+    }
+    
+    public String MOTIVS() {
+        return MOTIVS;
+    }
+    
+	  public int DZIVSK() {
+        return DZIVSK;
+    }
+	  
+	  public int HP() {
+	        return HP;
+	    }
+	  
+	  public int SPEKS() {
+	        return SPEKS;
+	    }
+	  
+	  public int AIZS() {
+	        return VECUMS;
+	    }
+
+	  public int VECUMS() {
+	        return VECUMS;
+	    }
 }
